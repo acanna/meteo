e5e4910f20c6051a9a56b3afa7526956122f42cc
Diamindes <DiamindFaceit@yandex.ru>
2017-10-19 05:45:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class Main { // TODO: Вызывает System(лишний класс)
+    public static void main(String[] args) {
+        System system = new System();
+        system.searchData();
+    }
+}
