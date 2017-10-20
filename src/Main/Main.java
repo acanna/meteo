b9b77272f0ce815c64ed71feeabcdff37d439a56
Diamind <DiamindFaceit@yandex.ru>
2017-10-21 02:56:41 +0300

    Version 3.9

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,12 +7,9 @@ public class Main {
         java.lang.System.out.println("      [Processing start]");
         System system = new System();
         double workTimeBegin = java.lang.System.nanoTime() / 1000000000.0;
-        LinkedList<String> badImagesList = system.searchData();
+        system.searchData();
         double workTimeEnd = java.lang.System.nanoTime() / 1000000000.0;
-        java.lang.System.out.println("_______________________\n      [Done]");
         java.lang.System.out.println("Total time - " + (workTimeEnd - workTimeBegin));
-        java.lang.System.out.println("Список необработанных изображений: ");
-        for(String fName : badImagesList)
-            java.lang.System.out.println(fName);
+        java.lang.System.out.println("Полный лог обработки смотрите Log.txt");
     }
 }
--- src/Main/System.java
+++ src/Main/System.java
@@ -7,41 +7,62 @@ import Filters.DataFilter;
 import Streams.Input;
 import Streams.Output;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 
 class System {
 
-  private ImageData image = new ImageData();
-  private DataFilter dataFilter = new DataFilter();
-  //  Инициализируем создание списка файлов изображений
-  private Input in =new Input();
-  private Output out = new Output();
-  //  Кол-во картинок
-  private int size = in.getSize();
-  // Список необработанных изображений
-  private LinkedList<String> badImage = new LinkedList<>();
-  //  Выволняет контроль за обработкой всех изображений
-  LinkedList<String> searchData(){
-      int index = 0;
-      while(index++ < size) {
-          try {
-              //  Открываем изображение, считывает пиксили
-              image.openImage(in.next());
-              java.lang.System.out.println("_________________________\nProcessing: \"" + in.getName() + "\"");
-              double proccTimeBegin = java.lang.System.nanoTime() / 1000000000.0;
-              //  Обработка и вывод в файл
-              // getPath - путь к файлу с данными
-              out.flush(dataFilter.getData(image.getPixelMap(), image.getImage(),in.getDir()),in.getPath());
+    private ImageData image = new ImageData();
+    private DataFilter dataFilter = new DataFilter();
+    //  Инициализируем создание списка файлов изображений
+    private Input in = new Input();
+    private Output out = new Output();
+    //  Кол-во картинок
+    private int size = in.getSize();
+    // Строка для лога проверки
+    private StringBuilder logStr = new StringBuilder();
 
-              double proccTimeEnd = java.lang.System.nanoTime() / 1000000000.0;
-              java.lang.System.out.println("Processing time - " + (proccTimeEnd - proccTimeBegin));
-              java.lang.System.out.println("COMPLETE");
-          }catch (BrokenImage e){
-              java.lang.System.err.println(e.getMessage());
-              badImage.addLast("    " + in.getName() + "  Причина: " + e.getMessage());
-              java.lang.System.err.println("ABORT");
-          }
-      }
-      return badImage;
-  }
+    //  Выволняет контроль за обработкой всех изображений
+    void searchData() {
+        int index = 0;
+        int ok=0, error =0;
+        while (index++ < size) {
+            try {
+                //  Открываем изображение, считывает пиксили
+                image.openImage(in.next());
+                java.lang.System.out.println("_________________________\nProcessing: \"" + in.getName() + "\"");
+                double proccTimeBegin = java.lang.System.nanoTime() / 1000000000.0;
+                //  Обработка и вывод в файл
+                // getPath - путь к файлу с данными
+                out.flush(dataFilter.getData(image.getPixelMap(), image.getImage(), in.getDir()), in.getPath());
+
+                double proccTimeEnd = java.lang.System.nanoTime() / 1000000000.0;
+                java.lang.System.out.println("Processing time - " + (proccTimeEnd - proccTimeBegin));
+                logStr.append(in.getName() + " - OK \n");
+                ++ok;
+                java.lang.System.out.println("COMPLETE");
+            } catch (BrokenImage e) {
+                java.lang.System.err.println(e.getMessage());
+                logStr.append(in.getName() + " - Error " + "  Причина: " + e.getMessage() + "\n");
+                ++error;
+                java.lang.System.err.println("ABORT");
+            }
+        }
+        java.lang.System.out.println("_______________________\n      [Done]");
+        java.lang.System.out.println("Всего изображений: " + (ok + error) + "\nOk - " + ok + "\nError - " + error);
+        logStr.append("_______________________\nВсего изображений: " + (ok + error) + "\n Ok - " + ok + "\n Error - " + error);
+        try {
+            BufferedWriter outLog = new BufferedWriter(new FileWriter(new File("Log.txt")));
+            for(String str : logStr.toString().split("\n")) {
+                outLog.write(str);
+                outLog.newLine();
+            }
+            outLog.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
